ac707152e4beff0af9e1050173342cba6268315a
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-11-11 19:59:33 +0100

    style: applying style to the doughnut chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DoughnutChartComponent],
+  imports: [RouterOutlet, DoughnutChartComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Chartjs';
 
-  displayTab: string = 'doughnut';
+  public displayTab: string = 'doughnut';
 
   updateTab(str: string) {
     this.displayTab = str;
