4b1e210051058fae3b3ef90d466f70d94cc26373
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-11-11 19:16:18 +0100

    feat: add styled tab navigation using Bootstrap for chart components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import Chart from 'chart.js/auto';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DoughnutChartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'chartjs-with-angular';
-  chart: any = [];
+  title = 'Chartjs';
 
-  ngOnInit() {
-    this.chart = new Chart('doughnut', {
-      type: 'doughnut',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1,
-            backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)',
-            ],
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
-      },
-    });
+  displayTab: string = 'doughnut';
+
+  updateTab(str: string) {
+    this.displayTab = str;
   }
 }
--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-doughnut-chart',
   standalone: true,
   imports: [],
   templateUrl: './doughnut-chart.component.html',
-  styleUrl: './doughnut-chart.component.scss'
+  styleUrl: './doughnut-chart.component.scss',
 })
 export class DoughnutChartComponent {
-
+  chart: any = [];
+  ngAfterViewInit() {
+    new Chart('doughnut', {
+      type: 'doughnut',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3],
+            borderWidth: 1,
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+            ],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        },
+      },
+    });
+  }
 }
